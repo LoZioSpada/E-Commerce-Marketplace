79e561fe799ff1af318f93d431956ac43dd14d12
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-10-06 19:59:56 +0200

    E-Commerce Marketplace
    - Popolato (finalmente) la pagina con i prodotti da me creati grazie al form nel backoffice;
    - Aggiunto dello stile alle card;

--- backoffice/backoffice.js
+++ backoffice/backoffice.js
@@ -6,19 +6,23 @@ const description = document.querySelector('#description')
 const brand = document.querySelector('#brand')
 const imageUrl = document.querySelector('#imageUrl')
 const price = document.querySelector('#price')
+const productForm =  document.querySelector('#productForm')
 
-const url = "https://striveschool-api.herokuapp.com/api/product/"
+let myHeaders = new Headers();
+
+
 
 
 
 
 // FUNZIONE PER ANDARE AD AGGIUNGERE UN PRODOTTO
-const addToMain = async (event) => {
+async function addToMain (event) {
     event.preventDefault()
-    const response = await fetch(url, {
-        method: 'POST',
+    const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+        method: "POST",
         headers: {
-            "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNTM5YzM5MzI3YzAwMThkM2EyZTQiLCJpYXQiOjE2OTYzNTUyMjgsImV4cCI6MTY5NzU2NDgyOH0.C0j8PM2EFXdAsuRqqVA3LRQieDwSkG5G22Z_UDkCoJQ'
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNTM5YzM5MzI3YzAwMThkM2EyZTQiLCJpYXQiOjE2OTYzNTUyMjgsImV4cCI6MTY5NzU2NDgyOH0.C0j8PM2EFXdAsuRqqVA3LRQieDwSkG5G22Z_UDkCoJQ",
+            "Content-Type": "application/json"
         },
         body: JSON.stringify({
             name: name.value,
@@ -26,8 +30,10 @@ const addToMain = async (event) => {
             brand: brand.value,
             imageUrl: imageUrl.value,
             price: price.value,
+            
         })
     })
+
     if (response.ok) {    //CONTROLLO STATO
         alert("Your product has been successfully added!")
         for (const field of [name, description, brand, imageUrl, price]) {
@@ -37,6 +43,8 @@ const addToMain = async (event) => {
         console.error("Something went wrong!")
     }
 }
+
+productForm.addEventListener('submit', addToMain)
 
 
 // FUNZIONE PER FARE LOGOUT DALL'ACCOUNT ADMIN
--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 // QUI ANDREMO AD INSERIRE TUTTE LE VARIABILI GLOBALI
-const url = "https://striveschool-api.herokuapp.com/api/product/"
 
 let contenuto = document.querySelector('.products .row')
 
 
-// //CARICAMENTO DELLA PAGINA
-// contenuto.innerHTML = /*html*/
-//     `
-// <div class="container pc">
-// 	<div class="loader"></div>
-// 	<div class="loader"></div>
-// 	<div class="loader"></div>
-// </div>
+// CARICAMENTO DELLA PAGINA
+//  contenuto.innerHTML = /*html*/
+//      `
+//  <div class="container pc">
+//  	<div class="loader"></div>
+//  	<div class="loader"></div>
+//  	<div class="loader"></div>
+//  </div>
 
-// `
+//  `
 
 
 
 // FUNZIONE CHE ANDRÀ A POPOLARE LA PAGINA CON CIÒ CHE IO ANDRÒ AD INSERIRE NELL'ENDPOINT
 const getProducts = async () => {
     try {
-        const response = await fetch(url, {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
             method: 'GET',
             headers: {
                 "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTFjNTM5YzM5MzI3YzAwMThkM2EyZTQiLCJpYXQiOjE2OTYzNTUyMjgsImV4cCI6MTY5NzU2NDgyOH0.C0j8PM2EFXdAsuRqqVA3LRQieDwSkG5G22Z_UDkCoJQ'
@@ -37,17 +36,17 @@ const getProducts = async () => {
 
 
 const displayProduct = (result) => {
-    contenuto.innerHTML = result.map((result) => {
+    contenuto.innerHTML = result.map(({name, description, brand, imageUrl, price, _id}) => {
         // CREAZIONE CARD CHE ANDRANNO A POPOLARE LA PAGINA
         return /*html*/ `
         <div class="col-6 col-md-3">
-                    <div class="card mb-5" id="product_${result.id}">
-                        <img src='${result.imageUrl}' class="card-img-top">
+                    <div class="card mb-5" id="product_${_id}">
+                        <img src='${imageUrl}' class="card-img-top">
                         <div class="card-body p-1">
-                            <h5 class="card-title">${result.name}</h5>
-                            <p class="card-text">Brand: ${result.brand}</p>
-                            <p class="card-text">${result.description}</p>
-                            <p class="card-text"> Price: ${result.price} €</p>
+                            <h5 class="card-title">${name}</h5>
+                            <p class="card-text">Brand: ${brand}</p>
+                            <p class="card-text">${description}</p>
+                            <p class="card-text"> Price: ${price} €</p>
                             <div class="d-flex justify-content-around align-items-center pb-1 px-1">
                                <!-- <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
                                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
@@ -55,14 +54,14 @@ const displayProduct = (result) => {
                                 LASCIATO COMMENTATO PER COMODITA' PER RIPRENDERLO DOPO (BACKOFFICE),
                                 L'UTENTE NON VEDE IL TASTO PER ELIMINARE UN PRODOTTO DALLA PAGINA -->
 
-                                <a href="dettagli/dettagli.html?id=${result.id}">
+                                <a href="dettagli/dettagli.html?id=${_id}">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                 <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
                                 </a>
 
-                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16" onclick="aggiungiAlCarrello('${result.name}', '${result.price}', '${result.id}' )">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16" onclick="aggiungiAlCarrello('${name}', '${price}', '${_id}' )">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                                 </svg>
@@ -71,7 +70,7 @@ const displayProduct = (result) => {
                     </div>
                 </div> `
     }).join('')
-    .finally(() => { contenuto.querySelector('.pc').remove() })
+    // .finally(() => { contenuto.querySelector('.pc').remove() })
 }
 
 
@@ -86,7 +85,7 @@ window.onload = async () => {
 }
 
 // FUNZIONE PER AGGIUNGERE AL CARRELLO I PRODOTTI SELEZIONATI
-const aggiungiAlCarrello = (name, price, id) => {
+const aggiungiAlCarrello = (name, price, _id) => {
     const product = document.querySelector('#product_' + id)
     product.style.border = '1px solid yellow'
     const cart = document.querySelector('.list')
@@ -102,7 +101,7 @@ const aggiungiAlCarrello = (name, price, id) => {
 
 
 // FUNZIONE PER RIMUOVERE IL CONTENUTO DAL CARRELLO
-const rimuoviDalCarrello = (event, id, price) => {
+const rimuoviDalCarrello = (event, _id, price) => {
     event.target.closest("li").remove()
     const totale = document.querySelector('.totalCart')
     totale.innerText = (Number(totale.innerText) - Number(price)).toFixed(2)
